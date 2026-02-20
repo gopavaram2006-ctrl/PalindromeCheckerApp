6e1fd1ee4ca3f5c0dfe3024bcacfaa398a47544c
gopavaram2006 <gopavaram2006@gmail.com>
2026-02-20 17:33:20 +0530

    UC7:Deque-Based Optimized Palindrome Checker

--- src/UseCase7PalindromeCheckerApp.java
+++ src/UseCase7PalindromeCheckerApp.java
@@ -1,27 +1,26 @@
 
-    import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+    import java.util.Deque;
+import java.util.ArrayDeque;
 
-    public class UseCase6PalindromeCheckerApp {
+    public class UseCase7PalindromeCheckerApp {
 
         public static void main(String[] args) {
 
-            String input = "level";
+            String input = "racecar";
 
-            Stack<Character> stack = new Stack<>();
-            Queue<Character> queue = new LinkedList<>();
+            Deque<Character> deque = new ArrayDeque<>();
 
             for (int i = 0; i < input.length(); i++) {
-                char ch = input.charAt(i);
-                stack.push(ch);
-                queue.add(ch);
+                deque.addLast(input.charAt(i));
             }
 
             boolean isPalindrome = true;
 
-            while (!stack.isEmpty()) {
-                if (stack.pop() != queue.remove()) {
+            while (deque.size() > 1) {
+                char front = deque.removeFirst();
+                char rear = deque.removeLast();
+
+                if (front != rear) {
                     isPalindrome = false;
                     break;
                 }
