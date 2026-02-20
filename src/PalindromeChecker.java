0a90927e0de4262d88b4f3a4b29dde748c2b8305
gopavaram2006 <gopavaram2006@gmail.com>
2026-02-20 17:01:03 +0530

    Base application setup

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,16 @@
+public class PalindromeChecker {
+
+
+        public static void main(String[] args) {
+
+            System.out.println("====================================");
+            System.out.println("     PALINDROME CHECKER APP");
+            System.out.println("====================================");
+            System.out.println("Application Version : 1.0");
+            System.out.println("Welcome to the Palindrome Checker Application!");
+            System.out.println("This application validates whether a given string is a palindrome.");
+            System.out.println("====================================");
+
+        }
+    }
+
